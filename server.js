d9c935b34349360f293727f878bbc50db3aaf738
Micky Rathod <mickyrathod@gmail.com>
2021-02-18 23:51:11 +1100

    Pizza42 challenge 1/2 baked

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const { join } = require("path");
+const authConfig = require("./auth_config.json");
+
+const app = express();
+
+if (!authConfig.domain || !authConfig.audience) {
+  throw "Please make sure that auth_config.json is in place and populated";
+}
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.static(join(__dirname, "public")));
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithms: ["RS256"]
+});
+//external endpoint
+app.get("/api/external", checkJwt, (req, res) => {
+    // save the order to your user's profile  
+    if (!req.user['https://example.com/verified']) {
+        res.send({
+            msg: "Please Verify your email before placing an order"
+        });
+    } else {
+        res.send({
+            msg: "Thanks for ordering Pizza 42"
+        });
+    }
+
+});
+
+app.get("/auth_config.json", (req, res) => {
+  res.sendFile(join(__dirname, "auth_config.json"));
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
+});
+
+process.on("SIGINT", function() {
+  process.exit();
+});
+
+module.exports = app;
